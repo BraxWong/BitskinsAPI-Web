a6e770e925f53197c73e391fe189286ec7630674
Brax Wong <ghostenjob@gmail.com>
2024-01-06 15:32:08 +0000

    The system will now check if the email is already in db before calling insertData endpoint

--- public/js/SignUp.js
+++ public/js/SignUp.js
@@ -1,26 +1,60 @@
-function sign_up(){
-  $.ajax({
-    url: '/startDatabase',
-    type: 'GET',
-    success: function(data) {
-      console.log(data);
-    }
-  });
-  const dataToInsert = {
-    email: document.getElementById("Login-Email").value,
-    password: document.getElementById("Login-Password").value
-  };
-  $.ajax({
-	  url: '/insertData',
-	  type: 'POST',
-	  contentType: 'application/json',
-  	data: JSON.stringify(dataToInsert),
-	  success: function(data) {
-      console.log(data);
-	  },
-	  error: function(error) {
-      console.error(error);
-	  }
-  });
+$.ajax({
+  url: '/startDatabase',
+  type: 'GET',
+  success: function(data) {
+    console.log(data);
+    databaseLoggedOn = true;
+  }
+});
+
+function directToLoginPage()
+{
+  alert("This email has already been registered, directing you to the login page");
+  window.location.href = '/login';
 }
 
+function insertUserCredentialsToDB(dataToInsert)
+{
+  $.ajax({
+    url: '/insertData',
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify(dataToInsert),
+    success: function(data, textStatus, xhr) {
+      alert("You account has been registered");
+    },
+    error: function(data, textStatus, xhr){
+      alert("You account has not been registered");
+    }
+  })  
+}
+
+function sign_up(){
+  var email = document.getElementById("Login-Email").value;
+  var password = document.getElementById("Login-Password").value;
+  if(password.length >= 8) {
+    const dataToInsert = {
+      email: email, 
+      password: password 
+    };
+    $.ajax({
+      url: '/findUser',
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify(dataToInsert),
+      success: function(data, textStatus, xhr) {
+        if(data.exists == true){
+          directToLoginPage();
+        } else {
+         insertUserCredentialsToDB(dataToInsert); 
+        }
+      },
+      error: function(xhr, textStatus) {
+        console.log(xhr.status);
+        alert(xhr.status);
+      }
+    });
+  } else {
+    alert("The password has to be at least 8 characters long");
+  }
+}
