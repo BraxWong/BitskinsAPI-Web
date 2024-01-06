489d08d446bd61b1e0f0b78407ab656965f13fab
Brax Wong <ghostenjob@gmail.com>
2024-01-06 15:37:28 +0000

    Updating database.js to reflect the real one

--- public/js/Node/dummy-database.js
+++ public/js/Node/dummy-database.js
@@ -17,7 +17,9 @@
 
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const bodyParser = require("body-parser")
 const uri = "mongodb+srv://<username>:<password>@<database_name>.jlm59ue.mongodb.net/?retryWrites=true&w=majority";
+var activateDB = false;
 var app = express();
 
 const client = new MongoClient(uri, {
@@ -30,14 +32,16 @@ const client = new MongoClient(uri, {
 
 
 app.get("/startDatabase", function(request, response) {
-  async function run() {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  if(activateDB == false) {
+    async function run() {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    run().catch(console.dir);
   }
-  run().catch(console.dir);
 })
 
 const dataToInsert = {
@@ -48,9 +52,14 @@ const dataToInsert = {
 const db = client.db('DATABASE_NAME');
 const collection = db.collection('DATABASE_COLLECTION_NAME');
 
-app.get("/insertData", function(request, response) {
-  if(request.body.email != "" | request.body.password != "") {
-    collection.insertOne(request.body);
+app.get("/insertData", async function(request, response) {
+  const result = collection.insertOne(request.body);
+  if (result) {
+    console.log("User Registered successfully");
+    response.status(200).json({success: true});
+  } else {
+    console.lig("Unable to register");
+    response.status(500).json({success: false});
   }
 })
 
@@ -58,5 +67,16 @@ app.get("/terminate", function(request, response) {
   client.close();
   console.log("Database has been terminated.");
 })
+
+app.get("/findUser", async function(request, response) {
+  const result = await collection.findOne({email: request.body.email});
+  if(result) {
+    console.log("User found:", result);
+    response.status(200).json({exists:true});
+  } else {
+    console.log("User not found:", result);
+    response.status(200).json({exists:false});
+  }
+});
 
 module.exports = app;
