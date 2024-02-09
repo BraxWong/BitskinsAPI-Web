99faf05b053f8392519d3c044042da3a69a77d7e
Brax Wong <ghostenjob@gmail.com>
2024-02-09 21:02:11 +0000

    Added config to sidenav

--- public/js/form.js
+++ public/js/form.js
@@ -1,7 +1,18 @@
-var dropdown = document.getElementsByClassName("dropdown-btn");
-var i;
-for(i = 0; i < dropdown.length; ++i) {
-  dropdown[i].addEventListener("click", function() {
+var account_dropdown = document.getElementsByClassName("account-dropdown-btn");
+var config_dropdown = document.getElementsByClassName("config-dropdown-btn");
+for(var i = 0; i < account_dropdown.length; ++i) {
+  account_dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
+for(var i = 0; i < config_dropdown.length; ++i) {
+  config_dropdown[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var dropdownContent = this.nextElementSibling;
     if (dropdownContent.style.display === "block") {
