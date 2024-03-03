a0f17d693e8eb78f08b4eb8182ec0c2d4a4c1bfd
Brax Wong <ghostenjob@gmail.com>
2024-03-03 15:53:35 +0000

    function: The website is now able to directly communicate with Bitskins API.

--- main.js
+++ main.js
@@ -1,146 +1,219 @@
-var express = require('express');
-var axios = require('axios');
+var express = require("express");
+var axios = require("axios");
 var app = express();
-var fs = require('fs');
-var database = require('./public/js/Node/database.js');
-var update_account = require('./public/schemas/update_account.schema.json');
-var update_tradelink = require('./public/schemas/update_tradelink_schema.json');
-var bitSkins_api = require('./public/schemas/api.schema.json');
-var historic_rewards = require('./public/schemas/historic_rewards.schema.json');
-var modify_affiliate_code = require('./public/schemas/set_change_affiliate_code.schema.json');
-var bitSkins_auth_token = require('./public/schemas/auth_token.schema.json');
-var verify_2FA_creation = require('./public/schemas/verify_2FA_creation.schema.json');
-var disable_2FA = require('./public/schemas/disable_2FA.schema.json');
-var verify_2FA_disable = require('./public/schemas/verify_disable_2FA.schema.json');
-var get_sales = require('./public/schemas/get_sales.schema.json');
-var get_pricing_summary = require('./public/schemas/get_pricing_summary.json');
-var tf2_market = require('./public/schemas/tf2_market.schema.json');
-var dota2_market = require('./public/schemas/dota2_market.schema.json');
-var cs2_market = require('./public/schemas/cs2_market.schema.json');
-var rust_market = require('./public/schemas/rust_market.schema.json');
-var user_store = require('./public/schemas/user_store.schema.json');
-var get_item_details = require('./public/schemas/get_item_details.schema.json');
-var search_skin = require('./public/schemas/search_skin.schema.json');
-var filters = require('./public/schemas/filters.schema.json');
-var buy_single_item = require('./public/schemas/buy_single_item.schema.json');
-var buy_multiple_items = require('./public/schemas/buy_multiple_items.schema.json');
-var buy_bulk_items = require('./public/schemas/buy_bulk_items.schema.json');
-var withdraw_single_item = require('./public/schemas/withdraw_single_item.schema.json');
-var withdraw_multiple_items = require('./public/schemas/withdraw_multiple_items.schema.json');
-var delist_single_item = require('./public/schemas/delist_single_item.schema.json');
-var delist_multiple_items = require('./public/schemas/delist_multiple_items.schema.json');
-var relist_single_item = require('./public/schemas/relist_single_item.schema.json');
-var relist_multiple_items= require('./public/schemas/relist_multiple_items.schema.json');
-var update_single_item = require('./public/schemas/update_single_item.schema.json');
-var update_multiple_items = require('./public/schemas/update_multiple_items.schema.json');
-var get_item_history = require('./public/schemas/get_item_history.schema.json');
-var get_items_history = require('./public/schemas/get_items_history.schema.json');
-var get_receipt = require('./public/schemas/get_receipt.schema.json');
-var bump_single_item = require('./public/schemas/bump_single_item.schema.json');
-var get_bumped_items = require('./public/schemas/get_bumped_items.schema.json');
-var enable_bumping = require('./public/schemas/enable_bumping.schema.json');
-var disable_bumping = require('./public/schemas/disable_bumping.schema.json');
-var buy_bump_packages = require('./public/schemas/buy_bump_packages.schema.json');
-var get_steam_inventory = require('./public/schemas/get_steam_inventory.schema.json');
-var deposit_steam_items = require('./public/schemas/deposit_steam_items.schema.json');
-var get_steam_trades = require('./public/schemas/get_steam_trades.schema.json');
-var get_active_steam_trades = require('./public/schemas/get_active_steam_trades.schema.json');
-var get_wallet_transactions = require('./public/schemas/get_wallet_transactions.schema.json');
-var get_wallet_pending_transactions = require('./public/schemas/get_wallet_pending_transactions.schema.json');
-var get_wallet_reports = require('./public/schemas/get_wallet_reports.schema.json');
-var generate_wallet_report = require('./public/schemas/generate_wallet_report.schema.json');
-var download_wallet_report = require('./public/schemas/download_wallet_report.schema.json');
-var deposit_binance = require('./public/schemas/deposit_binance.schema.json');
-var get_litecoin_address = require('./public/schemas/get_litecoin_address.schema.json');
-var get_bitcoin_address = require('./public/schemas/get_bitcoin_address.schema.json');
-var use_gift_code = require('./public/schemas/use_gift_code.schema.json');
-var deposit_zen = require('./public/schemas/deposit_zen.schema.json');
-var add_card = require('./public/schemas/add_card.schema.json');
-var deposit_card = require('./public/schemas/deposit_card.schema.json');
-var withdraw_bitcoin = require('./public/schemas/withdraw_bitcoin.schema.json');
-var withdraw_litecoin = require('./public/schemas/withdraw_litecoin.schema.json');
-var withdraw_ethereum = require('./public/schemas/withdraw_ethereum.schema.json');
-var withdraw_binance = require('./public/schemas/withdraw_binance.schema.json');
-var withdraw_visa = require('./public/schemas/withdraw_visa.schema.json');
-var schemas_array = [update_account, update_tradelink, bitSkins_api, historic_rewards, modify_affiliate_code, bitSkins_auth_token,
-                     verify_2FA_creation, disable_2FA, verify_2FA_disable, get_sales, get_pricing_summary, tf2_market, dota2_market,
-                     cs2_market, rust_market, user_store, get_item_details, search_skin, filters, buy_single_item, buy_multiple_items,
-                     buy_bulk_items, withdraw_single_item, withdraw_multiple_items, delist_single_item, delist_multiple_items,
-                     relist_single_item, relist_multiple_items, update_single_item, update_multiple_items, get_items_history,
-                     get_item_details, get_receipt, bump_single_item, get_bumped_items, enable_bumping, disable_bumping, buy_bump_packages,
-                     get_steam_inventory, deposit_steam_items, get_steam_trades, get_active_steam_trades, get_wallet_transactions, get_wallet_pending_transactions,
-                     get_wallet_reports, generate_wallet_report, download_wallet_report, deposit_binance, get_litecoin_address, get_bitcoin_address,
-                     use_gift_code, deposit_zen, add_card, deposit_card, withdraw_bitcoin, withdraw_litecoin, withdraw_ethereum, withdraw_binance,
-                     withdraw_visa];
-const kill = require('kill-port')
+var fs = require("fs");
+var bodyParser = require("body-parser");
+var database = require("./public/js/Node/database.js");
+var update_account = require("./public/schemas/update_account.schema.json");
+var update_tradelink = require("./public/schemas/update_tradelink_schema.json");
+var bitSkins_api = require("./public/schemas/api.schema.json");
+var historic_rewards = require("./public/schemas/historic_rewards.schema.json");
+var modify_affiliate_code = require("./public/schemas/set_change_affiliate_code.schema.json");
+var bitSkins_auth_token = require("./public/schemas/auth_token.schema.json");
+var verify_2FA_creation = require("./public/schemas/verify_2FA_creation.schema.json");
+var disable_2FA = require("./public/schemas/disable_2FA.schema.json");
+var verify_2FA_disable = require("./public/schemas/verify_disable_2FA.schema.json");
+var get_sales = require("./public/schemas/get_sales.schema.json");
+var get_pricing_summary = require("./public/schemas/get_pricing_summary.json");
+var tf2_market = require("./public/schemas/tf2_market.schema.json");
+var dota2_market = require("./public/schemas/dota2_market.schema.json");
+var cs2_market = require("./public/schemas/cs2_market.schema.json");
+var rust_market = require("./public/schemas/rust_market.schema.json");
+var user_store = require("./public/schemas/user_store.schema.json");
+var get_item_details = require("./public/schemas/get_item_details.schema.json");
+var search_skin = require("./public/schemas/search_skin.schema.json");
+var filters = require("./public/schemas/filters.schema.json");
+var buy_single_item = require("./public/schemas/buy_single_item.schema.json");
+var buy_multiple_items = require("./public/schemas/buy_multiple_items.schema.json");
+var buy_bulk_items = require("./public/schemas/buy_bulk_items.schema.json");
+var withdraw_single_item = require("./public/schemas/withdraw_single_item.schema.json");
+var withdraw_multiple_items = require("./public/schemas/withdraw_multiple_items.schema.json");
+var delist_single_item = require("./public/schemas/delist_single_item.schema.json");
+var delist_multiple_items = require("./public/schemas/delist_multiple_items.schema.json");
+var relist_single_item = require("./public/schemas/relist_single_item.schema.json");
+var relist_multiple_items = require("./public/schemas/relist_multiple_items.schema.json");
+var update_single_item = require("./public/schemas/update_single_item.schema.json");
+var update_multiple_items = require("./public/schemas/update_multiple_items.schema.json");
+var get_item_history = require("./public/schemas/get_item_history.schema.json");
+var get_items_history = require("./public/schemas/get_items_history.schema.json");
+var get_receipt = require("./public/schemas/get_receipt.schema.json");
+var bump_single_item = require("./public/schemas/bump_single_item.schema.json");
+var get_bumped_items = require("./public/schemas/get_bumped_items.schema.json");
+var enable_bumping = require("./public/schemas/enable_bumping.schema.json");
+var disable_bumping = require("./public/schemas/disable_bumping.schema.json");
+var buy_bump_packages = require("./public/schemas/buy_bump_packages.schema.json");
+var get_steam_inventory = require("./public/schemas/get_steam_inventory.schema.json");
+var deposit_steam_items = require("./public/schemas/deposit_steam_items.schema.json");
+var get_steam_trades = require("./public/schemas/get_steam_trades.schema.json");
+var get_active_steam_trades = require("./public/schemas/get_active_steam_trades.schema.json");
+var get_wallet_transactions = require("./public/schemas/get_wallet_transactions.schema.json");
+var get_wallet_pending_transactions = require("./public/schemas/get_wallet_pending_transactions.schema.json");
+var get_wallet_reports = require("./public/schemas/get_wallet_reports.schema.json");
+var generate_wallet_report = require("./public/schemas/generate_wallet_report.schema.json");
+var download_wallet_report = require("./public/schemas/download_wallet_report.schema.json");
+var deposit_binance = require("./public/schemas/deposit_binance.schema.json");
+var get_litecoin_address = require("./public/schemas/get_litecoin_address.schema.json");
+var get_bitcoin_address = require("./public/schemas/get_bitcoin_address.schema.json");
+var use_gift_code = require("./public/schemas/use_gift_code.schema.json");
+var deposit_zen = require("./public/schemas/deposit_zen.schema.json");
+var add_card = require("./public/schemas/add_card.schema.json");
+var deposit_card = require("./public/schemas/deposit_card.schema.json");
+var withdraw_bitcoin = require("./public/schemas/withdraw_bitcoin.schema.json");
+var withdraw_litecoin = require("./public/schemas/withdraw_litecoin.schema.json");
+var withdraw_ethereum = require("./public/schemas/withdraw_ethereum.schema.json");
+var withdraw_binance = require("./public/schemas/withdraw_binance.schema.json");
+var withdraw_visa = require("./public/schemas/withdraw_visa.schema.json");
+var schemas_array = [
+  update_account,
+  update_tradelink,
+  bitSkins_api,
+  historic_rewards,
+  modify_affiliate_code,
+  bitSkins_auth_token,
+  verify_2FA_creation,
+  disable_2FA,
+  verify_2FA_disable,
+  get_sales,
+  get_pricing_summary,
+  tf2_market,
+  dota2_market,
+  cs2_market,
+  rust_market,
+  user_store,
+  get_item_details,
+  search_skin,
+  filters,
+  buy_single_item,
+  buy_multiple_items,
+  buy_bulk_items,
+  withdraw_single_item,
+  withdraw_multiple_items,
+  delist_single_item,
+  delist_multiple_items,
+  relist_single_item,
+  relist_multiple_items,
+  update_single_item,
+  update_multiple_items,
+  get_items_history,
+  get_item_details,
+  get_receipt,
+  bump_single_item,
+  get_bumped_items,
+  enable_bumping,
+  disable_bumping,
+  buy_bump_packages,
+  get_steam_inventory,
+  deposit_steam_items,
+  get_steam_trades,
+  get_active_steam_trades,
+  get_wallet_transactions,
+  get_wallet_pending_transactions,
+  get_wallet_reports,
+  generate_wallet_report,
+  download_wallet_report,
+  deposit_binance,
+  get_litecoin_address,
+  get_bitcoin_address,
+  use_gift_code,
+  deposit_zen,
+  add_card,
+  deposit_card,
+  withdraw_bitcoin,
+  withdraw_litecoin,
+  withdraw_ethereum,
+  withdraw_binance,
+  withdraw_visa,
+];
+const kill = require("kill-port");
 
-app.use('/public', express.static(__dirname + '/public'));
-app.use('/', database);
+app.use("/public", express.static(__dirname + "/public"));
+app.use("/", database);
+app.use(bodyParser.json());
 console.log("You are in the right place");
 
-app.get('/main', function(request, response) {
-  fs.readFile("public/html/main.html", function(err, text) {
+app.get("/main", function (request, response) {
+  fs.readFile("public/html/main.html", function (err, text) {
     response.setHeader("Content-Type", "text/html");
     response.end(text);
   });
-})
+});
 
-app.get('/contact-us', function(request, response) {
-  fs.readFile("public/html/contact-us.html", function(err, text) {
+app.get("/contact-us", function (request, response) {
+  fs.readFile("public/html/contact-us.html", function (err, text) {
     response.setHeader("Content-Type", "text/html");
     response.end(text);
   });
-})
+});
 
-app.get('/login', function(request, response) {
-  fs.readFile("public/html/Login.html", function(err, text) {
+app.get("/login", function (request, response) {
+  fs.readFile("public/html/Login.html", function (err, text) {
     response.setHeader("Content-Type", "text/html");
     response.end(text);
   });
-})
+});
 
-app.get('/forgot-password', function(request, response) {
-  fs.readFile("public/html/forgot-password.html", function(err, text) {
+app.get("/forgot-password", function (request, response) {
+  fs.readFile("public/html/forgot-password.html", function (err, text) {
     response.setHeader("Content-Type", "text/html");
     response.end(text);
   });
-})
+});
 
-app.get('/signup', function(request, response) {
-  fs.readFile("public/html/SignUp.html", function(err, text) {
+app.get("/signup", function (request, response) {
+  fs.readFile("public/html/SignUp.html", function (err, text) {
     response.setHeader("Content-Type", "text/html");
     response.end(text);
-  })
-})
+  });
+});
 
-app.get('/profile-settings', function(request, response) {
-  fs.readFile("public/html/profile-settings.html", function(err, text) {
+app.get("/profile-settings", function (request, response) {
+  fs.readFile("public/html/profile-settings.html", function (err, text) {
     response.setHeader("Content-Type", "text/html");
     response.end(text);
-  })
-})
+  });
+});
 
-app.post('/get-schema', function(request, response) {
+app.post("/get-schema", function (request, response) {
   var array_index = JSON.parse(request.body.num);
-  if(array_index <= schemas_array.length - 1) {
-    response.header("Content-Type", 'application/json');
+  if (array_index <= schemas_array.length - 1) {
+    response.header("Content-Type", "application/json");
     response.status(200).json(schemas_array[array_index]);
   } else {
-    response.status(500).json({error: "The schema is not found"});
+    response.status(500).json({ error: "The schema is not found" });
+  }
+});
+
+app.post("/send-form", function (request, response) {
+  if(request.body.type === "GET")
+  {
+    axios
+      .get("https://api.bitskins.com" + request.body.url, {
+        headers: {
+          "content-type": "application/json",
+          "x-apikey": request.body.form.API,
+        },
+      })
+      .then((result) => response.send(result.data))
+      .catch((error) => response.send(error.response.data));
+  } else {
+    axios.post("https://api.bitskins.com" + request.body.url, request.body.form, {
+      "headers": {
+        "content-type": "application/json",
+        "x-apikey": request.body.form.API,
+      },
+    })
+    .then(result => console.log('Request success', result.data))
+    .catch(error => console.error('Request failed', error.response.data));
   }
 });
 
 app.listen(8080, "127.0.0.1", () => {
-  process.on('exit', () => {
+  process.on("exit", () => {
     // Kill the port when the process is about to exit
-    kill(8080, 'tcp')
-      .then(console.log)
-      .catch(console.log);
+    kill(8080, "tcp").then(console.log).catch(console.log);
   });
-  
-  process.on('SIGINT', () => {
+
+  process.on("SIGINT", () => {
     process.exit(0);
   });
 });
-
-
--- public/js/form.js
+++ public/js/form.js
@@ -1,115 +1,185 @@
 var account_dropdown = document.getElementsByClassName("account-dropdown-btn");
 var profile_dropdown = document.getElementsByClassName("profile-dropdown-btn");
-var affiliate_dropdown = document.getElementsByClassName("affiliate-dropdown-btn");
-var two_factor_dropdown = document.getElementsByClassName("two-factor-dropdown-btn");
+var affiliate_dropdown = document.getElementsByClassName(
+  "affiliate-dropdown-btn",
+);
+var two_factor_dropdown = document.getElementsByClassName(
+  "two-factor-dropdown-btn",
+);
 var api_dropdown = document.getElementsByClassName("api-dropdown-btn");
 var config_dropdown = document.getElementsByClassName("config-dropdown-btn");
 var market_dropdown = document.getElementsByClassName("market-dropdown-btn");
 var pricing_dropdown = document.getElementsByClassName("pricing-dropdown-btn");
-var market_item_dropdown = document.getElementsByClassName("market-items-dropdown-btn");
-var buy_items_dropdown = document.getElementsByClassName("buy-items-dropdown-btn");
-var withdraw_dropdown = document.getElementsByClassName("withdraw-dropdown-btn");
+var market_item_dropdown = document.getElementsByClassName(
+  "market-items-dropdown-btn",
+);
+var buy_items_dropdown = document.getElementsByClassName(
+  "buy-items-dropdown-btn",
+);
+var withdraw_dropdown = document.getElementsByClassName(
+  "withdraw-dropdown-btn",
+);
 var delist_dropdown = document.getElementsByClassName("delist-dropdown-btn");
 var relist_dropdown = document.getElementsByClassName("relist-dropdown-btn");
-var update_price_dropdown = document.getElementsByClassName("update-price-dropdown-btn");
-var item_history_dropdown = document.getElementsByClassName("item-history-dropdown-btn");
+var update_price_dropdown = document.getElementsByClassName(
+  "update-price-dropdown-btn",
+);
+var item_history_dropdown = document.getElementsByClassName(
+  "item-history-dropdown-btn",
+);
 var receipt_dropdown = document.getElementsByClassName("receipt-dropdown-btn");
-var all_available_skin_dropdown = document.getElementsByClassName("all-available-skins-dropdown-btn");
-var all_insell_items_dropdown = document.getElementsByClassName("all-insell-items-dropdown-btn");
+var all_available_skin_dropdown = document.getElementsByClassName(
+  "all-available-skins-dropdown-btn",
+);
+var all_insell_items_dropdown = document.getElementsByClassName(
+  "all-insell-items-dropdown-btn",
+);
 var steam_dropdown = document.getElementsByClassName("steam-dropdown-btn");
-var steam_trades_dropdown = document.getElementsByClassName("steam-trades-dropdown-btn");
+var steam_trades_dropdown = document.getElementsByClassName(
+  "steam-trades-dropdown-btn",
+);
 var wallet_dropdown = document.getElementsByClassName("wallet-dropdown-btn");
 var bump_up_dropdown = document.getElementsByClassName("bump-up-dropdown-btn");
-var wallet_stats_dropdown = document.getElementsByClassName("wallet-stats-dropdown-btn");
-var wallet_transactions_dropdown = document.getElementsByClassName("wallet-transactions-dropdown-btn");
-var wallet_reports_dropdown = document.getElementsByClassName("wallet-reports-dropdown-btn");
-var wallet_deposit_dropdown = document.getElementsByClassName("wallet-deposit-dropdown-btn");
-var cryptocurrency_dropdown = document.getElementsByClassName("cryptocurrency-dropdown-btn");
+var wallet_stats_dropdown = document.getElementsByClassName(
+  "wallet-stats-dropdown-btn",
+);
+var wallet_transactions_dropdown = document.getElementsByClassName(
+  "wallet-transactions-dropdown-btn",
+);
+var wallet_reports_dropdown = document.getElementsByClassName(
+  "wallet-reports-dropdown-btn",
+);
+var wallet_deposit_dropdown = document.getElementsByClassName(
+  "wallet-deposit-dropdown-btn",
+);
+var cryptocurrency_dropdown = document.getElementsByClassName(
+  "cryptocurrency-dropdown-btn",
+);
 var binance_dropdown = document.getElementsByClassName("binance-dropdown-btn");
-var giftcode_dropdown = document.getElementsByClassName("giftcode-dropdown-btn");
+var giftcode_dropdown = document.getElementsByClassName(
+  "giftcode-dropdown-btn",
+);
 var zen_dropdown = document.getElementsByClassName("zen-dropdown-btn");
 var card_dropdown = document.getElementsByClassName("card-dropdown-btn");
-var wallet_withdraw_dropdown = document.getElementsByClassName("wallet-withdraw-dropdown-btn");
-var wallet_withdraw_cryptocurrency_dropdown = document.getElementsByClassName("wallet-withdraw-cryptocurrency-dropdown-btn");
-var wallet_withdraw_binance_dropdown = document.getElementsByClassName("wallet-withdraw-binance-dropdown-btn");
+var wallet_withdraw_dropdown = document.getElementsByClassName(
+  "wallet-withdraw-dropdown-btn",
+);
+var wallet_withdraw_cryptocurrency_dropdown = document.getElementsByClassName(
+  "wallet-withdraw-cryptocurrency-dropdown-btn",
+);
+var wallet_withdraw_binance_dropdown = document.getElementsByClassName(
+  "wallet-withdraw-binance-dropdown-btn",
+);
 var visa_dropdown = document.getElementsByClassName("visa-dropdown-btn");
-var dropdownarr = [account_dropdown, profile_dropdown, affiliate_dropdown, two_factor_dropdown, api_dropdown, config_dropdown, market_dropdown, pricing_dropdown, 
-                   market_item_dropdown, buy_items_dropdown, withdraw_dropdown, delist_dropdown, relist_dropdown, update_price_dropdown, item_history_dropdown, 
-                   receipt_dropdown, bump_up_dropdown, all_available_skin_dropdown, all_insell_items_dropdown, steam_dropdown, steam_trades_dropdown, wallet_dropdown, 
-                   wallet_stats_dropdown, wallet_transactions_dropdown, wallet_reports_dropdown, wallet_deposit_dropdown, cryptocurrency_dropdown, binance_dropdown, 
-                   giftcode_dropdown, zen_dropdown, card_dropdown, wallet_withdraw_dropdown, wallet_withdraw_cryptocurrency_dropdown, wallet_withdraw_binance_dropdown, visa_dropdown];
+var results_text_area = document.getElementById("request-results");
+var dropdownarr = [
+  account_dropdown,
+  profile_dropdown,
+  affiliate_dropdown,
+  two_factor_dropdown,
+  api_dropdown,
+  config_dropdown,
+  market_dropdown,
+  pricing_dropdown,
+  market_item_dropdown,
+  buy_items_dropdown,
+  withdraw_dropdown,
+  delist_dropdown,
+  relist_dropdown,
+  update_price_dropdown,
+  item_history_dropdown,
+  receipt_dropdown,
+  bump_up_dropdown,
+  all_available_skin_dropdown,
+  all_insell_items_dropdown,
+  steam_dropdown,
+  steam_trades_dropdown,
+  wallet_dropdown,
+  wallet_stats_dropdown,
+  wallet_transactions_dropdown,
+  wallet_reports_dropdown,
+  wallet_deposit_dropdown,
+  cryptocurrency_dropdown,
+  binance_dropdown,
+  giftcode_dropdown,
+  zen_dropdown,
+  card_dropdown,
+  wallet_withdraw_dropdown,
+  wallet_withdraw_cryptocurrency_dropdown,
+  wallet_withdraw_binance_dropdown,
+  visa_dropdown,
+];
 var form = document.getElementById("form");
 var currentSchema = {};
 var editor = "";
 var currentURL = "/account/profile/me";
-var endPointMethod = "GET"
+var endPointMethod = "GET";
 
 $.ajax({
-  url: '/startDatabase',
-  type: 'GET',
-  success: function(data) {
+  url: "/startDatabase",
+  type: "GET",
+  success: function (data) {
     console.log(data);
     databaseLoggedOn = true;
   },
-  error: function(xhr,status,error) {
+  error: function (_xhr, _status, error) {
     console.error(error);
-  }
+  },
 });
 
 showForm(2, currentURL, endPointMethod);
 
-function returnUserData(){
+function returnUserData() {
   return new Promise((resolve, reject) => {
     $.ajax({
-      url: '/userData',
-      type: 'GET',
-      success: function(data){
+      url: "/userData",
+      type: "GET",
+      success: function (data) {
         resolve(data.apiKey);
       },
-      error: function(xhr,status,error){
+      error: function (_xhr, _status, error) {
         reject(error);
-      }
+      },
     });
   });
 }
 
-function showForm(schemaNum, endPoint, method)
-{
+function showForm(schemaNum, endPoint, method) {
   form.innerHTML = "";
   currentURL = endPoint;
-  endPoint = method; 
+  endPoint = method;
   $.ajax({
-    url: '/get-schema',
-    type: 'POST',
+    url: "/get-schema",
+    type: "POST",
     contentType: "application/json",
-    data: JSON.stringify({"num": schemaNum}),
-    success: function(data) {
+    data: JSON.stringify({ num: schemaNum }),
+    success: function (data) {
       currentSchema = data;
       var config = {
         use_name_attributes: false,
-        theme: 'bootstrap4',
+        theme: "bootstrap4",
         disable_edit_json: true,
         disable_properties: true,
         disable_collapse: true,
         schema: currentSchema,
       };
       editor = new JSONEditor(form, config);
-      if(JSON.stringify(currentSchema).includes("API")){
-        editor.on('ready', ()=> {
-          try{
-            returnUserData().then(data => {
-              editor.setValue({"API": data});
+      if (JSON.stringify(currentSchema).includes("API")) {
+        editor.on("ready", () => {
+          try {
+            returnUserData().then((data) => {
+              editor.setValue({ API: data });
             });
-          } catch (error){
+          } catch (error) {
             console.error(error);
           }
-        })
+        });
       }
-    }
+    },
   });
 }
 
-function testing(){
+function testing() {
   alert("TESTING");
 }
 
@@ -119,12 +189,12 @@ function testing(){
 //                        ┃                             ┃
 //                        ╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯
 
-for(var i = 0; i < dropdownarr.length; ++i) {
-  for(var j = 0; j < dropdownarr[i].length; ++j){
-    dropdownarr[i][j].addEventListener("click", function() {
+for (var i = 0; i < dropdownarr.length; ++i) {
+  for (var j = 0; j < dropdownarr[i].length; ++j) {
+    dropdownarr[i][j].addEventListener("click", function () {
       this.classList.toggle("active");
       var dropdownContent = this.nextElementSibling;
-      if(dropdownContent.style.display === "block") {
+      if (dropdownContent.style.display === "block") {
         dropdownContent.style.display = "none";
       } else {
         dropdownContent.style.display = "block";
@@ -133,4 +203,22 @@ for(var i = 0; i < dropdownarr.length; ++i) {
   }
 }
 
-
+function submitForm() {
+  var jsonBody = {
+    url: currentURL,
+    type: endPointMethod, 
+    form: editor.getValue(),
+  };
+  $.ajax({
+    url: "/send-form",
+    type: "POST",
+    contentType: "application/json",
+    data: JSON.stringify(jsonBody),
+    success: function (data) {
+      results_text_area.innerText = JSON.stringify(data);
+    },
+    error: function (_xhr, _status, error) {
+      results_text_area.innerText = error;
+    },
+  });
+}
